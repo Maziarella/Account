c10deca3f795306843d43fe8ef341174acda6fe1
Maziarella <kiebala.magdalena@gmail.com>
2020-06-22 08:14:23 +0200

    Implement AccountDbContext class.

---
+++ WebAPI/DataContext/AccountDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.DataContext
+{
+    public class AccountDbContext : DbContext
+    {
+
+       
+            public AccountDbContext(DbContextOptions<AccountDbContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<Models.Account> Account { get; set; }
+    }
+}
