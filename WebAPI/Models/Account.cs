eab589f88385e1aa26d1f9607bc45daf94050a52
Maziarella <kiebala.magdalena@gmail.com>
2020-06-22 08:22:24 +0200

    Implement Account model.

--- WebAPI/Models/Account.cs
+++ WebAPI/Models/Account.cs
@@ -7,6 +7,10 @@ namespace WebAPI.Models
 {
     public class Account
     {
-
+        public int IdAccount { get; set; }
+        public int AccountNumber { get; set; }
+        public string AccountName { get; set; }
+        public decimal AvailableFunds { get; set; }
+        public decimal Blance { get; set; }
     }
 }
